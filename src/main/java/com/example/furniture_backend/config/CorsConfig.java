dc719410675b2db63e683f8866a8480875515806
abcom <abcom@DESKTOP-O9LBR4I>
2026-06-20 00:24:14 +0530

    Fix Cors

--- src/main/java/com/example/furniture_backend/config/CorsConfig.java
+++ src/main/java/com/example/furniture_backend/config/CorsConfig.java
@@ -1,12 +1,12 @@
 package com.example.furniture_backend.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.cors.CorsConfigurationSource;
-
-import java.util.List;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class CorsConfig {
